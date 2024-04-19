e2d00c9217abf3161561fc3ae26841beab0e34e5
Anny (Char) <lop6581111@gmail.com>
2024-04-18 20:47:39 -0400

    library versions
    (stole the idea from shadowmario lol)

---
+++ setup/readme.md
@@ -0,0 +1,2 @@
+hey fun fact these use older library versions
+MAKE SURE YOU RUN THIS FIRST BEFORE YOU COMPILE
---
+++ setup/setup-windows.bat
@@ -0,0 +1,17 @@
+@echo off
+color 0a
+echo INSTALLING/SETTING LIBRARIES
+@echo on
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
+haxelib set flixel 5.2.2
+haxelib set flixel-tools 1.5.1
+haxelib set flixel-addons 3.0.2
+haxelib set flixel-ui 2.5.0
+haxelib set sscript 5.2.0
+haxelib set hxcodec 2.6.1
+haxelib set lime 8.0.1
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
+haxelib set openfl 9.2.1
+@echo off
+echo DONE
+pause
