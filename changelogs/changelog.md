350e7b4b5d4dc8abdab15f9b8db59a24b6148b5d
Anny (Char) <73309364+gameygu-0213@users.noreply.github.com>
2024-06-28 17:02:53 -0400

    New Versioning System
    as in imma actually properly version this lmao

--- changelogs/changelog.md
+++ changelogs/changelog.md
@@ -9,10 +9,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 # VS Char Revitalized Changelog
 
-## [Alpha-1/Engine 0.6.x - 0.9.x]
+## [Alpha-1.1.5/Engine 0.9h]
+
+### Added
+
+### Changed
+
+- How i versioned shit lmao
+
+## [Alpha 1 - 1.1.4/Engine 0.6.x - 0.9h]
 
 - I'll split the Engine from the mod later lmao.
-- also yeah im just lumping all the engine versions together here, fuck you
 - 5/13/24 THE REHAULING OF ALL TIME! so much stuff oh my god 
 - 5/17/24 GODDAMN IT STILL HASNT STOPPED GETTING BIGGER WTF
 - 6/6/24 YEP STILL GETTIN' BIGGER
--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -27,7 +27,7 @@ class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = " Psych Engine v0.7.1h | Funkin' 0.2.8"; // Just cause. its fun to tell people im using an older version of psych, with some things from 0.7.3 i backported
 	public static var charEngineVersion:String = '0.9h | THE SPLIT VOCALS UPDATE'; // Used for making sure im not an idiot, and properly update the engine version lmao.
-	public static var vsCharVersion:String = 'Alpha 1 | THE GREAT OVERHAUL'; // Used for updating
+	public static var vsCharVersion:String = 'Alpha 1.1.5 | THE GREAT OVERHAUL'; // Used for updating
 	public static var discordRPCString:String;
 	public static var curSelected:Int = 0;
 	public var MenuOptionImage = new FlxSprite().loadGraphic(Paths.image('menuimage'));
